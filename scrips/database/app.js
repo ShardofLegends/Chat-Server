af493eee76c4046475be87068284e1a7c6743b61
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2024-02-22 22:43:05 +0100

    Registration works but doesn't serve a new html

--- scrips/database/app.js
+++ scrips/database/app.js
@@ -1,40 +1,50 @@
-const express = require('express');
+ 
+var express=require("express");
+var bodyParser=require("body-parser");
+ 
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = 3000;
-
-// MongoDB-Verbindung herstellen
-mongoose.connect('mongodb://localhost:27017/deineDatenbank', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Middleware für das Parsen von Anforderungskörpern
-app.use(bodyParser.urlencoded({ extended: true }));
+const path = require('path');
+mongoose.connect('mongodb+srv://manuel:px6GTna4g6jM8IVx@cluster0.hcanky9.mongodb.net/');
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+ 
+var app=express()
+ 
+ 
 app.use(bodyParser.json());
-
-// MongoDB-Schema für Benutzerregistrierungen
-const userSchema = new mongoose.Schema({
-  fullName: String,
-  username: String,
-  email: String,
-  password: String,
-});
-
-// MongoDB-Modell für Benutzer
-const User = mongoose.model('User', userSchema);
-
-// Endpunkt für Benutzerregistrierung
-app.post('/register', async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    const savedUser = await newUser.save();
-    res.json(savedUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Server starten
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+app.post('/sign_up', function(req,res){
+    var fullname = req.body.fullname;
+    var username =req.body.username;
+    var email = req.body.email;
+    var password =req.body.password;
+ 
+    var data = {
+        "fullname":fullname,
+        "username":username,
+        "email":email,
+        "password":password
+    }
+db.collection('User-Registrations').insertOne(data,function(err, collection){
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+             
+    });
+         
+    return res.redirect('index.html');
+})
+ 
+ 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../Interface/index.html'));
+}).listen(3000);
+ 
+ 
+console.log("server listening at port 3000");
--- scrips/login.js
+++ scrips/login.js
@@ -1,3 +1,1 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+
